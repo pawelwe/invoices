3168bfe730c77869e4a5e9d9ac370953de322192
Paweł Węgrzyniak <pawelwegrzyniak@MacBook-Pro-Pawe.local>
2017-06-22 19:45:59 +0200

    Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+'use strict';
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    resolve: {
+        extensions: ['.js', '.jsx', '.json']
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loaders: ['babel-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'postcss-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf|png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader'
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css'),
+        new HtmlWebpackPlugin({
+            template: 'dist/index.html',
+            inject: false,
+            minify: {
+                collapseWhitespace: true
+            }
+        })
+    ],
+    devServer: {
+        inline: true,
+        historyApiFallback: true,
+    },
+    devtool: 'source-map',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+};
